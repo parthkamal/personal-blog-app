4b4325e2d0bea17dcbef5cff0cfdc8b28b352102
parthkamal <kamalparth40@gmail.com>
2022-01-11 09:21:15 +0530

    started woring on the react admin dashboard

---
+++ admin-panel/src/pages/MainDashboard.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Navbar from '../components/Navbar';
+
+class Dashboard extends React.Component {
+    render() { 
+        return <div><Navbar/></div>;
+    }
+}
+ 
+export default Dashboard;
