6ff78155ce2f92190f21dc30dc1e0df5541a585e
parthkamal <kamalparth40@gmail.com>
2022-01-16 08:04:59 +0530

    styling of navbar and forms

--- admin-panel/src/components/Navbar.jsx
+++ admin-panel/src/components/Navbar.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 
 function Navbar(props) {
-    return ( <>
+  return (<>
     <nav>
-  <div className="logo"><a href="/">parth kamal</a></div>
-  <ul>
-    <a href="/about"><li>about</li></a>
-    <a href="/posts"><li>posts</li></a>
-    <a href="/resume"><li>resume</li></a>
-    <a href="/projects"><li>projects</li> </a>
-    <a href="/contact"><li>contact</li> </a>
-    <a href="http://localhost:9000/"><li>admin</li> </a>
-  </ul>
-</nav>
+      <div className="logo"><a href="/">parth kamal</a></div>
+      <ul className='navbar-admin'>
+        <a href="/about"><li>about</li></a>
+        <a href="/posts"><li>posts</li></a>
+        <a href="/resume"><li>resume</li></a>
+        <a href="/projects"><li>projects</li> </a>
+        <a href="/contact"><li>contact</li> </a>
+      </ul>
+    </nav>
 
-    </> );
-    
+  </>);
+
 }
 
 export default Navbar;
