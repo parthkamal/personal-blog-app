4450d75a86ddd0d1ebf324faf5fe2afe8159f5c6
parthkamal <kamalparth40@gmail.com>
2022-01-16 19:48:27 +0530

    styled pages on the client side

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,76 +1,72 @@
 import express from "express";
+import fetch from "node-fetch";
 const router = express.Router();
 
-const post = {
-    _id: "ffosdghsdb34196592",
-    image: "image url",
-    date: "date string",
-    author: "parth kamal",
-    title: "this is a post title",
-    summary:
-      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be",
-    description: [
-      "this is point 1",
-      "this is point 2",
-      "this is point 2",
-      "this is point 2",
-      "this is point 3",
-    ],
-  };
-  const article = {
-    title: "hello i am parth kamal this is my blog website",
-    description:
-      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.<ul><li>this is a point",
-  };
-  
-  const hyperlinks = [
-    {
-      title: "github",
-      link: "https://github.com/parthkamal",
-    },
-    {
-      title: "github",
-      link: "https://github.com/parthkamal",
-    },
-    {
-      title: "github",
-      link: "https://github.com/parthkamal",
-    },
-    {
-      title: "github",
-      link: "https://github.com/parthkamal",
-    },
-  ];
-  
+const fetchObject = async (url) => {
+  try {
+    const object = await fetch(url)
+      .then((res) => res.json())
+      .then((json) => json);
+    return object;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
+const hyperlinks = [
+  {
+    title: "github",
+    link: "https://github.com/parthkamal",
+  },
+  {
+    title: "github",
+    link: "https://github.com/parthkamal",
+  },
+  {
+    title: "github",
+    link: "https://github.com/parthkamal",
+  },
+  {
+    title: "github",
+    link: "https://github.com/parthkamal",
+  },
+];
+
 //apis
 router.get("/", (req, res) => {
-    res.render("pages/home", { article: article });
-  });
-  
-  router.get("/about", (req, res) => {
-    res.render("pages/about", { article: article });
-  });
-  
-  router.get("/resume", (req, res) => {
-    res.render("pages/resume");
-  });
-  
-  router.get("/projects", (req, res) => {
-    res.render("pages/projects");
-  });
-  
-  router.get("/posts", async (req, res) => {
-    res.render("pages/posts", { post: post });
-  });
-  
-  router.get("/contact", (req, res) => {
-    res.render("pages/contact", { hyperlinks: hyperlinks });
-  });
-  
-  router.post("/contact", (req, res) => {
-    console.log(req.body);
-    res.redirect("/contact");
-  });
+  res.render("pages/home");
+});
 
+router.get("/about", async (req, res) => {
+  const abouts = await fetchObject("http://localhost:8080/admin/about");
+  res.render("pages/about", { abouts: abouts });
+});
 
-  export default router;
+router.get("/resume", async (req, res) => {
+  const resumes = await fetchObject("http://localhost:8080/admin/resume");
+  console.log(resumes);
+  res.render("pages/resume",{resumes:resumes});
+});
+router.get("/projects", async(req, res) => {
+  const projects = await fetchObject("http://localhost:8080/admin/projects");
+  res.render("pages/projects", { projects: projects });
+});
+
+router.get("/posts", async (req, res) => {
+  const posts =await fetchObject('http://localhost:8080/admin/posts')
+  console.log(posts);
+  res.render("pages/post", { posts: posts });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("pages/contact", { hyperlinks: hyperlinks });
+});
+
+router.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.redirect("/contact");
+});
+
+export default router;
